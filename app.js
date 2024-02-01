668c633e26eaaba86582f891ecbd61541a81f749
Mosi <mosisu07@gmail.com>
2024-02-01 11:09:07 +0800

    updated: used axios to fetch API

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-let data = [
-  {
-    id: 0,
-    name: "肥宅心碎賞櫻3日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    area: "高雄",
-    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: "貓空纜車雙程票",
-    imgUrl:
-      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台北",
-    description:
-      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: "台中谷關溫泉會1日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台中",
-    description:
-      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
-// 表單區
+// {
+//   let data = [
+//     {
+//       id: 0,
+//       name: "肥宅心碎賞櫻3日",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
+//       area: "高雄",
+//       description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
+//       group: 87,
+//       price: 1400,
+//       rate: 10,
+//     },
+//     {
+//       id: 1,
+//       name: "貓空纜車雙程票",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+//       area: "台北",
+//       description:
+//         "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
+//       group: 99,
+//       price: 240,
+//       rate: 2,
+//     },
+//     {
+//       id: 2,
+//       name: "台中谷關溫泉會1日",
+//       imgUrl:
+//         "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+//       area: "台中",
+//       description:
+//         "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
+//       group: 20,
+//       price: 1765,
+//       rate: 7,
+//     },
+//   ];
+// }
+
+//表單區;
 const ticketName = document.querySelector("#ticketName");
 const ticketImgUrl = document.querySelector("#ticketImgUrl");
 const ticketRegion = document.querySelector("#ticketRegion");
@@ -48,6 +51,50 @@ const addTicketBtn = document.querySelector(".addTicket-btn");
 const ticketCardArea = document.querySelector(".ticketCard-area");
 const regionSearch = document.querySelector(".regionSearch");
 const searchResultText = document.querySelector("#searchResult-text");
+
+let url =
+  "https://raw.githubusercontent.com/hexschool/js-training/main/travelAPI-lv1.json";
+
+axios.get(url).then((res) => {
+  let data = res.data;
+
+  showCards(data);
+  updateItemCounts(data);
+  addTicketBtn.addEventListener("click", (e) => {
+    let newTicket = {};
+    newTicket.id = data.length + 1;
+    newTicket.name = e.target.parentNode[0].value;
+    newTicket.imgUrl = e.target.parentNode[1].value;
+    newTicket.area = e.target.parentNode[2].value;
+    newTicket.price = e.target.parentNode[3].value;
+    newTicket.group = e.target.parentNode[4].value;
+    newTicket.rate = e.target.parentNode[5].value;
+    newTicket.description = e.target.parentNode[6].value;
+    if (
+      newTicket.name == "" ||
+      newTicket.area == "" ||
+      newTicket.price == "" ||
+      newTicket.group == "" ||
+      newTicket.rate == "" ||
+      newTicket.description == ""
+    ) {
+      return alert("請完成所有必填欄位");
+    }
+    data.push(newTicket);
+    showCards(data);
+    updateItemCounts(data);
+    alert("新增成功");
+  });
+
+  regionSearch.addEventListener("change", (e) => {
+    let area = e.target.value;
+    let filter = data.filter((item) => {
+      return item.area.includes(area);
+    });
+    showCards(filter);
+    updateItemCounts(filter);
+  });
+});
 
 function showCards(ary) {
   let str = "";
@@ -90,39 +137,3 @@ function updateItemCounts(ary) {
   let item = ary.length;
   searchResultText.innerHTML = `本次搜尋共 ${item} 筆資料`;
 }
-
-showCards(data);
-addTicketBtn.addEventListener("click", (e) => {
-  let newTicket = {};
-  newTicket.id = data.length + 1;
-  newTicket.name = e.target.parentNode[0].value;
-  newTicket.imgUrl = e.target.parentNode[1].value;
-  newTicket.area = e.target.parentNode[2].value;
-  newTicket.price = e.target.parentNode[3].value;
-  newTicket.group = e.target.parentNode[4].value;
-  newTicket.rate = e.target.parentNode[5].value;
-  newTicket.description = e.target.parentNode[6].value;
-  if (
-    newTicket.name == "" ||
-    newTicket.area == "" ||
-    newTicket.price == "" ||
-    newTicket.group == "" ||
-    newTicket.rate == "" ||
-    newTicket.description == ""
-  ) {
-    return alert("請完成所有必填欄位");
-  }
-  data.push(newTicket);
-  showCards(data);
-  updateItemCounts(data);
-  alert("新增成功");
-});
-
-regionSearch.addEventListener("change", (e) => {
-  let area = e.target.value;
-  let filter = data.filter((item) => {
-    return item.area.includes(area);
-  });
-  showCards(filter);
-  updateItemCounts(filter);
-});
